ec2db8d93d693166e2dacf83f8a257eaee85ec66
Anton Sushchevskiy <anton.sushchevskiy@ladevelopers.com>
2020-04-15 22:00:11 +0300

    fix web picker

--- lib/src/image_picker_web.dart
+++ lib/src/image_picker_web.dart
@@ -6,10 +6,9 @@ import 'package:image_picker/image_picker.dart';
 import 'package:universal_image_picker/src/image_picker_interface.dart';
 
 class UniversalImagePicker implements ImagePickerInterface {
-  // Uint8List file;
-  StreamController<Uint8List> fileUploaded;
-
   Future<Uint8List> pickImage(ImageSource source) async {
+    final completer = Completer<Uint8List>();
+
     final InputElement uploadInput = querySelector('#fileloader');
     uploadInput.value = '';
     uploadInput.accept = 'image/*';
@@ -17,19 +16,17 @@ class UniversalImagePicker implements ImagePickerInterface {
     uploadInput.onChange.first.then((e) {
       final files = uploadInput.files;
       if (files.length != 1) {
-        fileUploaded.add(null);
+        completer.complete(null);
         return;
       }
       final file = files[0];
       final reader = FileReader();
 
       reader.onLoadEnd.listen((e) {
-        fileUploaded.add(reader.result);
+        completer.complete(reader.result);
       });
       reader.readAsArrayBuffer(file);
     });
-    Uint8List image = await fileUploaded.stream.take(1).single;
-    fileUploaded.close();
-    return image;
+    return completer.future;
   }
 }
